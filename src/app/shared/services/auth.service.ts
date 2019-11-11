1115d7052ad41946b180078233193f955b3529b8
flast1k <flast1k@gmail.com>
2019-11-11 20:27:20 +0300

    add firebase

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { User } from '../../admin/shared/interfaces';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { FbAuthResponse, User } from '../../admin/shared/interfaces';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  private setToken() {}
+  private setToken(response: FbAuthResponse) {
+    console.log(response);
+  }
 
   get token(): string {
     return '';
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+        user
+      )
+      .pipe(tap(this.setToken));
   }
 
   logout() {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './inteface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAsOtHph6mJNlylfPcquYRyor1tusyicP4'
 };
