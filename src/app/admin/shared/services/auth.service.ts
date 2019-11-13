9bb3ebd93c878c57c380d92b3bbee38b2d402286
flast1k <flast1k@gmail.com>
2019-11-13 21:08:34 +0300

    add a form for creating post

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, Subject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
-import { FbAuthResponse, User } from '../interfaces';
+import { FbAuthResponse, User } from '../../../shared/interfaces';
 import { environment } from '../../../../environments/environment';
 
 @Injectable()
