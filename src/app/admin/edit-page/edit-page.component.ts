f3933fc555b31c1280e69984527a812fa4a01291
flast1k <flast1k@gmail.com>
2019-11-15 21:21:28 +0300

    add logic for editing post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PostsService } from '../../shared/posts.service';
 import { switchMap } from 'rxjs/operators';
 import { Post } from '../../shared/interfaces';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  post: Post;
+  submitted = false;
+
+  updateSubscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +31,7 @@ export class EditPageComponent implements OnInit {
         })
       )
       .subscribe((post: Post) => {
+        this.post = post;
         this.form = new FormGroup({
           title: new FormControl(post.title, Validators.required),
           text: new FormControl(post.text, Validators.required)
@@ -33,5 +39,28 @@ export class EditPageComponent implements OnInit {
       });
   }
 
-  submit() {}
+  ngOnDestroy(): void {
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
+    }
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+
+    this.updateSubscription = this.postsService
+      .update({
+        ...this.post,
+        id: this.post.id,
+        text: this.form.value.text,
+        title: this.form.value.title
+      })
+      .subscribe(() => {
+        this.submitted = false;
+      });
+  }
 }
