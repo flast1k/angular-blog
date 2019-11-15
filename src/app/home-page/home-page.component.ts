adb42c57752a8e1ac96435253a811458151cbd55
flast1k <flast1k@gmail.com>
2019-11-15 22:05:25 +0300

    output posts on home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../shared/posts.service';
+import { Observable } from 'rxjs';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
@@ -6,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  constructor() {}
+  posts$: Observable<Post[]>;
 
-  ngOnInit() {}
+  constructor(private postService: PostsService) {}
+
+  ngOnInit() {
+    this.posts$ = this.postService.getAll();
+  }
 }
