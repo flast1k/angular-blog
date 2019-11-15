bf482df07e15299fd41da16e453d554f1c578757
flast1k <flast1k@gmail.com>
2019-11-15 20:28:44 +0300

    add list of posts output

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../shared/services/auth.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostsService } from '../../shared/posts.service';
+import { Post } from '../../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  postsSubscription: Subscription;
 
-  ngOnInit() {}
+  constructor(private postService: PostsService) {}
+
+  ngOnInit() {
+    this.postsSubscription = this.postService
+      .getAll()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
+  }
+
+  remove(id: string) {}
 }
